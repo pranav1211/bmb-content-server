34ec58214d99422f9b90de2f9db526c055dee0ac
pranavisda1 <pranavisda1@gmail.com>
2025-08-06 20:17:41 +0530

    06-08-2025

--- app/view-images/page.js
+++ app/view-images/page.js
@@ -105,7 +105,7 @@ export default function ViewImagesPage() {
     setSelectedSubCategory('all')
   }
 
-  const getImageUrl = (path) => `https://bmb-content-server.vercel.app/${path}`
+  const getImageUrl = (path) => `https://content.beyondmebtw.com/${path}`
 
   const copyToClipboard = async (text, type) => {
     try {
