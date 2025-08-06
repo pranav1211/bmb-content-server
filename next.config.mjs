907076a806e0bd1d8faf1d7fa04baf63295e5c13
pranavisda1 <pranavisda1@gmail.com>
2025-08-06 20:53:16 +0530

    06-08-2025

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'bmb-content-server.vercel.app',
+        hostname: 'content.beyondmebtw.com',
         pathname: '/**',
       },
     ],
